742a6a7217d41809fb6bbaa16212bba8c14117a8
Markus Hartmann <markush1986@gmail.com>
2018-09-15 13:04:42 +0200

    initial commit

---
+++ src/app/books/books.component.ts
@@ -0,0 +1,29 @@
+import { Component, OnInit } from '@angular/core';
+import { Book } from "../book"
+import { BOOKS } from '../mock-books';
+
+@Component({
+  selector: 'app-books',
+  templateUrl: './books.component.html',
+  styleUrls: ['./books.component.css']
+})
+export class BooksComponent implements OnInit {
+
+  book: Book = {
+    id: 23,
+    name: "Test"
+  }
+  books = BOOKS;
+
+  selectedBook: Book;
+
+  onSelect(book: Book): void {
+    this.selectedBook = book;
+  }
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
