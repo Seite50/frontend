68d5aa8d3787b38aa00509795441b81ebe57fee9
Markus Hartmann <markush1986@gmail.com>
2018-09-15 19:35:23 +0200

    mhhh

--- src/app/book-detail/book-detail.component.ts
+++ src/app/book-detail/book-detail.component.ts
@@ -4,6 +4,10 @@ import { Book } from '../book';
 import { ActivatedRoute } from '@angular/router';
 import { Location } from '@angular/common';
 import { BookService }  from '../book.service';
+import { AuthorService } from '../author.service';
+import { Author } from '../author';
+import {FormBuilder, FormControl, FormGroup, Validators} from '@angular/forms';
+
 @Component({
   selector: 'app-book-detail',
   templateUrl: './book-detail.component.html',
@@ -12,13 +16,27 @@ import { BookService }  from '../book.service';
 export class BookDetailComponent implements OnInit {
 
   @Input() book: Book;
+  author: Author;
+  authors: Author[]
+  bookForm: FormGroup;
 
   constructor(  private route: ActivatedRoute,
     private bookService: BookService,
+    private authorService: AuthorService,
+    private fb: FormBuilder,
     private location: Location) { }
 
   ngOnInit() {
     this.getBook();
+    this.getAuthors();
+    this.fb.group({bookControl: this.book.authors})
+    //this.bookForm = this.fb.group({
+   //   bookControl: this.book.authors
+   // });
+  }
+  
+  getAuthors(): void {
+    this.authorService.getAuthors().subscribe(authors => this.authors = authors)
   }
 
   getBook(): void {
@@ -32,6 +50,7 @@ export class BookDetailComponent implements OnInit {
   }
 
   save(): void {
+    console.info(this.book)
     this.bookService.updateBook(this.book)
       .subscribe(() => this.goBack());
   }
