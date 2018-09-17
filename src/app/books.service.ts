1af9044b7272ce7c17f9d5058a31af774733e9fb
Markus Hartmann <markush1986@gmail.com>
2018-09-17 18:26:24 +0200

    Standard-Services

--- src/app/books.service.ts
+++ src/app/books.service.ts
@@ -1,9 +1,22 @@
 import { Injectable } from '@angular/core';
+import { MessageService} from './message.service';
+import { HttpClient } from '@angular/common/http';
+import { GenericrestService } from './genericrest.service';
+import { Book } from './book';
 
 @Injectable({
   providedIn: 'root'
 })
-export class BooksService {
+export class BooksService extends GenericrestService<Book> {
 
-  constructor() { }
+
+  constructor(
+      private http: HttpClient
+    ) {
+      super(
+        http,
+        '/api/books'
+      );
+    }
+
 }
