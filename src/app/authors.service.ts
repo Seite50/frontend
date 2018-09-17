58a1a62fbc611a6144a0228720ba41c9dc93c109
Markus Hartmann <markush1986@gmail.com>
2018-09-17 22:10:53 +0200

    alle services implementiert

--- src/app/authors.service.ts
+++ src/app/authors.service.ts
@@ -1,9 +1,22 @@
 import { Injectable } from '@angular/core';
+import { MessageService} from './message.service';
+import { HttpClient } from '@angular/common/http';
+import { GenericrestService } from './genericrest.service';
+import { Author } from './author';
 
 @Injectable({
   providedIn: 'root'
 })
-export class AuthorsService {
+export class AuthorsService extends GenericrestService<Author> {
 
-  constructor() { }
+
+  constructor(
+      private http: HttpClient
+    ) {
+      super(
+        http,
+        '/api/authors'
+      );
+    }
+
 }
--- src/app/users.service.ts
+++ src/app/users.service.ts
@@ -1,9 +1,21 @@
 import { Injectable } from '@angular/core';
+import { MessageService} from './message.service';
+import { HttpClient } from '@angular/common/http';
+import { GenericrestService } from './genericrest.service';
+import { User } from './user';
 
 @Injectable({
   providedIn: 'root'
 })
-export class UsersService {
+export class UsersService extends GenericrestService<User> {
 
-  constructor() { }
+  constructor(
+      private http: HttpClient
+    ) {
+      super(
+        http,
+        '/api/books'
+      );
+    }
+
 }
